edccb0d79346e1a564fe92bbdb77460c58391b5e
Doug Andrzejewski <douglas_andrzejewski@ski-network.net>
2022-03-15 20:33:19 -0400

    Add project files.

---
+++ Pages/Shared/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_App_Google_Wall_Calendar.Pages.Shared
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Weather()
+        {
+            return View();
+        }
+    }
+}
